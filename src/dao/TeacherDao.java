d4d6f437a3e7516e1d88ec135fdd75cb57e63d6b
admin <admin@LAPTOP-LO600G7D>
2025-06-12 14:10:05 +0900

    ３時間もDaoに使っちゃってごめ～んねっ

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import Bean.School;
 import Bean.Teacher;
@@ -28,9 +29,10 @@ public class TeacherDao extends Dao {
             }
 
 //			schoolの検索
-            String sql2 = "SELECT * FROM school WHERE ORDER BY id ASC";
+            String sql2 = "SELECT * FROM school WHERE  ORDER BY id ASC";
             PreparedStatement st2 = con.prepareStatement(sql2);
             ResultSet rs2 = st2.executeQuery();
+            st.setInt(1, );
 
             School school = new School();
             if (rs.next()) {
@@ -47,23 +49,36 @@ public class TeacherDao extends Dao {
         }
 
     }
+//Teacher オブジェクトに関連する School 情報を設定するため
+//	getメソッド
+	public Teacher get(String id) throws Exception {
+		Teacher Teacher = new Teacher();
+//	sql作成
+		try (Connection con = getConnection()) {
+			String sql = "SELECT * FROM Teacher WHERE id = ?";
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, id);
+			ResultSet rs = st.executeQuery();
+//	sql結果受け取り
+			String teacher_id = rs.getString("id");
+			String teacher_password = rs.getString("password");
+			String teacher_name = rs.getString("name");
+			String teacher_school_cd = rs.getString("school_cd");
+//	beanにセット
+			Teacher.setId(teacher_id);
+			Teacher.setPassword(teacher_password);
+			Teacher.setName(teacher_name);
 
-    public Teacher findById(int id) throws Exception {
-        Teacher c = null;
+			School school = new School();
+			String sql2 = "SELECT * FROM SCHOOL WHERE SCHOOL_CD = ?";
+			PreparedStatement st2 = con.prepareStatement(sql2);
+			st2.setString(1, teacher_school_cd);
+	        ResultSet rs2 = st2.executeQuery();
+			Teacher.setSchool(teacher_school_cd);
+			String school_cd = rs2.getString("school_cd");
+			String school_name = rs2.getString("school_name");
 
-        try (Connection con = getConnection()) {
-            String sql = "SELECT * FROM school WHERE id = ?";
-            PreparedStatement st = con.prepareStatement(sql);
-            st.setInt(1, id);
-            ResultSet rs = st.executeQuery();
-
-            if (rs.next()) {
-                c = new Teacher();
-                c.setId(rs.getString("id"));
-                c.setName(rs.getString("name"));
-            }
-        }
-
-        return c;
-    }
+		}
+		return Teacher;
+	}
 }
