c2bc854ba4544087671e673d24b34eef22944a91
admin <admin@LAPTOP-LO600G7D>
2025-06-13 14:55:56 +0900

    土日やってくるすまん

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -1,7 +1,15 @@
 package dao;
 
+import Bean.School;
+import Bean.Student;
+import Bean.Test;
+
 public class TestDao extends Dao {
 
 	String basesql;
+
+	public Test get(Student student,Subject subject,School school,int no) throws Exception {
+
+	}
 
 }
